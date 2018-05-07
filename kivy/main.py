1e7bf975a8c64b0404d118a050b7e4d0693bad66
ChemicalInk <38759407+ChemicalInk@users.noreply.github.com>
2018-05-07 19:08:28 +0300

    Empty kivy app

---
+++ kivy/main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class GameWidget(Widget):
+    pass
+
+
+class SpiderSquasherApp(App):
+    def build(self):
+        pass
+
+
+if __name__ == '__main__':
+    SpiderSquasherApp().run()
